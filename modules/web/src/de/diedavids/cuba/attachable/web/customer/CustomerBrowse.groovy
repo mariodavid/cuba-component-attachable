4d990bc52be20fc32e453b58d5b5b9b7a6a2a141
Mario David <mariodavid@arcor.de>
2018-02-09 22:25:22 +0100

    removed customer example

--- modules/web/src/de/diedavids/cuba/attachable/web/customer/CustomerBrowse.groovy
+++
@@ -1,8 +0,0 @@
-package de.diedavids.cuba.attachable.web.customer
-
-import de.balvi.cuba.declarativecontrollers.web.browse.AnnotatableAbstractLookup
-import de.diedavids.cuba.attachable.web.HasAttachments
-
-@HasAttachments(listComponent = 'customersTable')
-class CustomerBrowse extends AnnotatableAbstractLookup {
-}
--- modules/web/src/de/diedavids/cuba/attachable/web/customer/CustomerEdit.groovy
+++
@@ -1,9 +0,0 @@
-package de.diedavids.cuba.attachable.web.customer
-
-import com.haulmont.cuba.gui.components.AbstractEditor
-import de.diedavids.cuba.attachable.example.Customer
-import de.diedavids.cuba.attachable.web.HasAttachments
-
-@HasAttachments
-class CustomerEdit extends AbstractEditor<Customer> {
-}
