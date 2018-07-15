ba2764d4c14dbab5803c6fa5ed912cd368a68d93
Mario <mario@die-davids.de>
2018-07-15 14:49:15 +0200

    added multi upload feature for attachments

--- modules/web/src/de/diedavids/cuba/attachable/web/attachment/AttachmentBrowse.groovy
+++ modules/web/src/de/diedavids/cuba/attachable/web/attachment/AttachmentBrowse.groovy
@@ -1,13 +1,19 @@
 package de.diedavids.cuba.attachable.web.attachment
 
 import com.haulmont.cuba.core.entity.Entity
+import com.haulmont.cuba.core.entity.FileDescriptor
 import com.haulmont.cuba.gui.WindowManager
 import com.haulmont.cuba.gui.WindowParam
+import com.haulmont.cuba.gui.app.core.file.MultiUploader
+import com.haulmont.cuba.gui.components.AbstractWindow
+import com.haulmont.cuba.gui.components.Frame
 import com.haulmont.cuba.gui.components.Table
+import com.haulmont.cuba.gui.components.Window
 import com.haulmont.cuba.gui.components.actions.CreateAction
 import com.haulmont.cuba.gui.data.CollectionDatasource
 import de.balvi.cuba.declarativecontrollers.web.browse.AnnotatableAbstractLookup
 import de.diedavids.cuba.attachable.entity.Attachment
+import de.diedavids.cuba.attachable.service.AttachmentService
 import de.diedavids.cuba.taggable.web.WithTags
 
 import javax.inject.Inject
@@ -27,6 +33,9 @@ class AttachmentBrowse extends AnnotatableAbstractLookup {
 
     @Inject
     CollectionDatasource<Attachment, UUID> attachmentsDs
+
+    @Inject
+    AttachmentService attachmentService
 
     @Override
     void init(Map<String, Object> params) {
@@ -58,5 +67,17 @@ class AttachmentBrowse extends AnnotatableAbstractLookup {
     void previewFile(Entity item, String columnId) {
         Attachment attachment = item as Attachment
         frame.openEditor('ddca$Attachment.preview', attachment, WindowManager.OpenType.DIALOG)
+    }
+
+    void multiupload() {
+        MultiUploader multiUploadDialog = openWindow("multiuploadDialog", WindowManager.OpenType.DIALOG) as MultiUploader
+        multiUploadDialog.addCloseWithCommitListener(new Window.CloseWithCommitListener() {
+            @Override
+            void windowClosedWithCommitAction() {
+                def filesToCreateAttachmentsFor = multiUploadDialog.files
+                attachmentService.createAttachments(entity, filesToCreateAttachmentsFor)
+                attachmentsDs.refresh()
+            }
+        })
     }
 }
